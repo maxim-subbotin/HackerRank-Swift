e72c43eac701167f47e85a85e9efbf6d9258a185
Maxim Subbotin <msubbotin@snappii.com>
2020-03-04 18:57:43 +0300

    Fix case

--- Hakerrank/Algorithms/FlatlandSpaceStationsTest.swift
+++ Hakerrank/Algorithms/FlatlandSpaceStationsTest.swift
@@ -9,11 +9,14 @@
 import Foundation
 
 class FlatlandSpaceStationsTest: PracticeTest {
-    let cityCount = 5
-    let spaceStations = [0, 4] // -> 2
+    /*let cityCount = 5
+    let spaceStations = [0, 4] // -> 2*/
     
     /*let cityCount = 6
     let spaceStations = [0, 1, 2, 4, 3, 5] // -> 0*/
+    
+    let cityCount = 20
+    let spaceStations = [13, 1, 11, 10, 6] // -> 6
     
     override func preExecuteAction() {
         print("Flatland Space Stations")
@@ -31,13 +34,13 @@ class FlatlandSpaceStationsTest: PracticeTest {
     
     func getMinDistance(forCityCount c: Int, andStations stations: [Int]) -> Int {
         var max = 0
-        var sorted = stations.sorted()
+        let sorted = stations.sorted()
         for i in -1..<sorted.count {
             var d = 0
             if i == -1 {
                 d = sorted[0]
             } else if i == sorted.count - 1 {
-                d = sorted.count - 1 - sorted[i]
+                d = c - 1 - sorted[i]
             } else {
                 d = (sorted[i + 1] - sorted[i]) / 2
             }
